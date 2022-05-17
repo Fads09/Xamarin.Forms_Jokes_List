157f0ce66640a8d9e3ab2a507f737d3b97b88548
Fadoua <faitamrou.90@gmail.com>
2022-05-17 12:11:46 -0400

    Initial commit

---
+++ Xamarin.Forms_Jokes_List/HomePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Xamarin.Forms_Jokes_List
+{
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
